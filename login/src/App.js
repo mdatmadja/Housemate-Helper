78713c1ee4033c1fc8193da7f8e0cc599125cd00
Ashvini Bhupatiraju <arbhupat@ucsc.edu>
2021-10-27 11:47:04 -0700

    added base files & main page

---
+++ login/src/App.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Housemate Manager
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
