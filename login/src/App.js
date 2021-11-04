ec5ef7e5c699d1d2bbdfeb34344d0c7329dfe79e
Ashvini Bhupatiraju <arbhupat@ucsc.edu>
2021-11-03 19:16:42 -0700

    Login & sign up with firebase authentication

--- login/src/App.js
+++ login/src/App.js
@@ -1,16 +1,42 @@
-import React, {useState, useEffect} from "react";
+import React, {Component} from "react";
 import './App.css';
+import fire from './fire';
+import Home from './Home.js'
+import Login from './Login.js'
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+    }
+
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({user});
+      }
+      else {
+        this.setState ({user: null});
+      }
+    })
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Housemate Manager
-        </h1>
-      </header>
+     {this.state.user ? (<Home/>) : (<Login/>)}
     </div>
   );
+}
 }
 
 export default App;
---
+++ login/src/fire.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import firebase from 'firebase';
+//import 'firebase/auth';
+//import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCs5pqXeqdIxcJ0_HLhlXJQh_2h_bCWK2Y",
+  authDomain: "login-c06a0.firebaseapp.com",
+  projectId: "login-c06a0",
+  storageBucket: "login-c06a0.appspot.com",
+  messagingSenderId: "658345143118",
+  appId: "1:658345143118:web:2a98c426d328d7c8fc4c1f"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;
